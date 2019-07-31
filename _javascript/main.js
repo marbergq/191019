5569bed869bfccf762eacad55391ce00b5421ae9
Martin Bergqvist <marbergq@gmail.com>
2019-07-31 15:08:09 +0200

    fix

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
 });
+
+window.openModal = (name) => {
+  console.log('modal', name);
+  const elm = document.getElementById(name);
+  elm.className += " is-active";
+  return false;
+}
+
+window.closeModal = (name) => {
+  console.log('modal', name);
+  const elm = document.getElementById(name);
+  elm.className =elm.className.replace("is-active",""); 
+  return false;
+}
